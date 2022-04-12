ca89256dde95fb90d90651ce26e8d65e9ad47999
ArchLeaders <archparliament@gmail.com>
2022-04-12 13:38:22 -0700

    Add project files.

---
+++ BotwScrips.Lib/Python.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotwScrips.Lib
+{
+    public class Python
+    {
+        public static async Task Call(string module, string args = "", string func = "[RUN.PYTHON]")
+        {
+            string downloadRoot = "";
+        }
+    }
+}
---
+++ BotwScrips.Lib/Vars.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json;
+
+namespace BotwScrips.Lib
+{
+    public class Vars
+    {
+        // static
+
+        public static string ConfigPath { get; set; } = $"{GetEnv("localappdata")}\\mtk\\config.json";
+
+        public static string? GetEnv(string var) => Environment.GetEnvironmentVariable(var);
+
+        public static dynamic GetConfig(string config)
+        {
+            if (!File.Exists(ConfigPath))
+                return "0:GetConfig:Error - Settings could not be found";
+
+            var json = JsonSerializer.Deserialize<Dictionary<string, dynamic>>(File.ReadAllText(ConfigPath));
+
+            if (json == null)
+                return "1:GetConfig:Error - Settings returned null";
+
+            return json[config];
+        }
+
+        // non-static
+
+        public Vars()
+        {
+            Config = JsonSerializer.Deserialize<Dictionary<string, dynamic>>(File.ReadAllText(ConfigPath)) ?? new();
+
+            // set configs
+            Python = Config["python"];
+        }
+
+        public Dictionary<string, dynamic> Config { get; set; } = new();
+
+        // loaded configs
+        public string Python { get; set; } = "";
+    }
+}
