9bfcf46ff8bfe47c78c5f013e1ec9deb008e69af
ArchLeaders <archparliament@gmail.com>
2022-04-12 22:28:00 -0700

    BNP Installer python script

---
+++ BotwScripts.Lib/Python/bnpinstaller.py
@@ -0,0 +1,23 @@
+﻿# python.exe bnpinstaller.py [true|false]
+
+from bcml.install import install_mod, link_master_mod
+from pathlib import Path
+from sys import argv as args
+
+def main():
+
+    if args.count > 3:
+        remerge: bool = args[2] == "true"
+    else:
+        remerge: bool = False
+
+    install_mod(
+        Path(args[1]),
+        merge_now=remerge
+    )
+
+    if remerge == True:
+        link_master_mod()
+
+if __name__ == '__main__':
+    main()
