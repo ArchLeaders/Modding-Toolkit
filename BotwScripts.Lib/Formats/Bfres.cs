22fc56352b91d94c6dcbf941634fd8e13b817c16
ArchLeaders <archparliament@gmail.com>
2022-04-12 22:13:20 -0700

    Add bfres wrapper (copied from BfresPlatformConverter)

---
+++ BotwScripts.Lib/Formats/Bfres.cs
@@ -0,0 +1,35 @@
+﻿using BfresLibrary;
+using Syroot.BinaryData;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Operations;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotwScripts.Lib.Formats
+{
+    /// <summary>
+    /// Copied from KillzXGaming's <a href="https://github.com/KillzXGaming/BfresPlatformConverter/blob/master/Program.cs">BfresPLatformConverter</a>
+    /// </summary>
+    public class Bfres
+    {
+        public static bool IsCompressed(string path)
+        {
+            using (var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read))
+            using (var reader = new BinaryDataReader(fileStream))
+            {
+                string magic = reader.ReadString(4, Encoding.ASCII);
+                return magic == "Yaz0";
+            }
+        }
+
+        public static ResFile LoadBfres(string bfresFile)
+        {
+            if (IsCompressed(bfresFile))
+                return new ResFile(new MemoryStream(Yaz0.Decompress(bfresFile)));
+            else
+                return new ResFile(bfresFile);
+        }
+    }
+}
