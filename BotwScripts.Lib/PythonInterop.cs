5125f52e8e11752098d7cf65065430fa68395643
ArchLeaders <archparliament@gmail.com>
2022-04-12 22:51:21 -0700

    Add python interop

---
+++ BotwScripts.Lib/PythonInterop.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Operations;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotwScripts.Lib
+{
+    public class PythonInterop
+    {
+        public PythonInterop(string module, params string[] args)
+        {
+            Mtk.UpdateScript(module);
+            Execute.App(Mtk.GetConfig("python"), $"{Mtk.StaticPath}\\Scripts\\{module}.py {string.Join(' ', args)}");
+        }
+    }
+}
