8fe148dab3b6e77b2d5c6f0f79312e74c3112770
ArchLeaders <archparliament@gmail.com>
2022-04-13 22:20:21 -0700

    Add common delegates

---
+++ BotwScripts.Lib/Delegates.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotwScripts.Lib
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public delegate string Input(string ask);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <returns></returns>
+    public delegate bool Option(string ask);
+
+    /// <summary>
+    /// 
+    /// </summary>
+    public delegate void Notify(object message);
+
+    public static class CLI
+    {
+        public static string Input(string ask)
+        {
+            Console.Write(ask);
+            var user = Console.ReadLine();
+
+            while (user == "" || user == null)
+            {
+                Console.Write($"Invalid responce -\n\n{ask}");
+                user = Console.ReadLine();
+            }
+
+            return user;
+        }
+    }
+}
