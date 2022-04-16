d073c986a17c3cd1ff042d3a56072de1925a2755
ArchLeaders <archparliament@gmail.com>
2022-04-16 02:11:32 -0700

    Basic report class

---
+++ BotwScripts.Lib/Reporter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BotwScripts.Lib
+{
+    public class Reporter
+    {
+        public string Title { get; set; }
+        public string Message { get; set; }
+        public string Stack { get; set; }
+        public int ID { get; set; }
+
+        public void PrintToConsole()
+        {
+
+        }
+
+        public Reporter(string message, string title = "Reported Exception", string stack = "", int id = 0)
+        {
+            Title = title;
+            Message = message;
+            Stack = stack;
+            ID = id;
+        }
+    }
+}
