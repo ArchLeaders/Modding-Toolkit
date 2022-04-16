4a39b15cddc5898e2c8ca6eb2a143e87f0e9d58b
ArchLeaders <archparliament@gmail.com>
2022-04-16 00:56:39 -0700

    Rename update function

--- BotwScripts.Lib/PythonInterop.cs
+++ BotwScripts.Lib/PythonInterop.cs
@@ -12,7 +12,7 @@ namespace BotwScripts.Lib
         public static bool HideOutput = false;
         public static async Task Call(string module, params string[] args)
         {
-            Mtk.UpdateScript(module);
+            Mtk.UpdateExternal(module);
             await Execute.App($"{Mtk.GetConfig("python")}\\python.exe", $"\"{Mtk.StaticPath}\\Scripts\\{module}\" {string.Join(' ', args)}", hidden: HideOutput, shellExecute: false);
         }
     }
